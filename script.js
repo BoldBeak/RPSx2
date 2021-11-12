f070a5881de7c919fb7af99781d1a7acfd6b7e05
boldbeak <boldbeakdev@gmail.com>
2021-11-11 18:43:00 -0600

    single round returns message as directed. Need to separate scoring into different function.

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ function computerCHOICE() {
 
 //get's human's selection (store in var)
 function playerCHOICE() {
-  //return CHOICE[Math.floor(Math.random() * CHOICE.length)];
+  //random choice generator for effecient testing
+  return CHOICE[Math.floor(Math.random() * CHOICE.length)];
   
-  let playerInput = prompt("Choose Rock, Paper, or Scissors");
+  //actual function for player input
+  /* let playerInput = prompt("Choose Rock, Paper, or Scissors");
 
   let testInput = playerInput.toLowerCase();
 
@@ -20,7 +22,7 @@ function playerCHOICE() {
     testInput = prompt(`Oops! Check your spelling and try again.
     
     Rock, Paper, or Scissors?`);
-  } 
+  }  */
   return testInput;
 }
 // Test output
@@ -28,7 +30,7 @@ function playerCHOICE() {
 //console.log(playerSelection);
 // works
 
-//play 1 round and record winner
+//play 1 round and record winner as a string
 
 function playRound(playerSelection, computerSelection) {
   let scoreStart = {
@@ -36,6 +38,7 @@ function playRound(playerSelection, computerSelection) {
       'computer': 0,
       'player': 0
     }
+
     
   let scoreRound = {};
   
@@ -44,25 +47,38 @@ function playRound(playerSelection, computerSelection) {
 
   console.log(playerSelection);
   console.log(computerSelection);
+
+  let winner = {
+    'tie': `It's a tie. You and the computer both chose ${playerSelection}.`,
+    'win': `You win! Your ${playerSelection} beats computer's ${computerSelection}. Let's play again!`,
+    'lose': `You lose! Computer's ${computerSelection} beat your ${playerSelection}. Try again!`
+  };
+  
+  let message = '';
   
   if (computerSelection == playerSelection) {
     scoreStart['tie'] += 1;
     scoreRound = scoreStart;
+    message = winner.tie;
   } else if (computerSelection == "rock" && playerSelection == "paper") {
     scoreStart['player'] += 1;
     scoreRound = scoreStart;
+    message = winner.win;
   } else if (computerSelection == 'paper' && playerSelection == 'scissors'){
     scoreStart['player'] += 1;
     scoreRound = scoreStart;
+    message = winner.win;
   } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
   scoreStart['player'] += 1;    
     scoreRound = scoreStart;
+    message = winner.win;
   } else {
     scoreStart.computer += 1;
     scoreRound = scoreStart;
+    message = winner.lose;
   }
   
-  return scoreRound;
+  return message;
 }
 
 console.log(playRound());
