b514548470c0d192f9102312b33b59ed1400195d
boldbeak <boldbeakdev@gmail.com>
2021-12-08 23:30:13 -0600

    game correctly plays through single round, adds to score, and displays winner of round.

--- script.js
+++ script.js
@@ -38,6 +38,24 @@ function computerCHOICE() {
   return CHOICE[Math.floor(Math.random() * CHOICE.length)];
 }
 
+let scoreStart = {
+  'tie': 0,
+  'player': 0,
+  'computer': 0
+};
+
+let tieTally = document.querySelector('#tieScore')
+let playerTally = document.querySelector('#playerScore');
+let computerTally = document.querySelector('#computerScore');
+
+tieTally.value = scoreStart.tie;
+playerTally.value = scoreStart.player;
+computerTally.value = scoreStart.computer;
+
+let messageBox = document.querySelector('.messageBox');
+let messageArea = document.querySelector('.messageArea');
+
+
 //play 1 round and record winner as a string
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerCHOICE();
@@ -53,89 +71,66 @@ function playRound(playerSelection, computerSelection) {
     'computer': `You lose! Computer's ${computerSelection} beat your ${playerSelection}. Try again!`
   };
   
-  let scoreStart = {
-    'tie': 0,
-    'player': 0,
-    'computer': 0
-  };
-
   let scoreRound = {};
-  
   
     if (computerSelection == playerSelection) {
       message = winner.tie;
       scoreStart.tie += 1;
       scoreRound = scoreStart;
+      tieTally.value = scoreRound.tie;
     } else if (computerSelection == "rock" && playerSelection == "paper") {
       message = winner.player;
       scoreStart.player += 1;
       scoreRound = scoreStart;
+      playerTally.value = scoreRound.player;
     } else if (computerSelection == 'paper' && playerSelection == 'scissors'){
       message = winner.player;
       scoreStart.player += 1;
       scoreRound = scoreStart;
+      playerTally.value = scoreRound.player;
     } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
       message = winner.player;
       scoreStart.player += 1;
       scoreRound = scoreStart;
+      playerTally.value = scoreRound.player;
     } else {
       message = winner.computer;
       scoreStart.computer += 1;
       scoreRound = scoreStart;
-    }
-  
-  console.log(scoreRound);
-  return message;
-};
-
-
-
-function game() {
-  let scoreStart = {
-    'tie': 0,
-    'computer': 0,
-    'player': 0
-  };
-
-  let scoreRound = {};
-  
-  function getScore() {
-    const result = playRound();
-    console.log(result);  
-
-    if (result.includes('tie')) {
-      scoreStart['tie'] += 1;
-      scoreRound = scoreStart;
-    } else if (result.includes('win')) {
-      scoreStart['player'] += 1;
-      scoreRound = scoreStart;
-    } else {
-      scoreStart.computer += 1;
-      scoreRound = scoreStart;
+      computerTally.value = scoreRound.computer;
     }
     
-    let roundResult = 
-    `  ~ Scores ~  
-    You've won: ${scoreRound.player} games.
-    Computer's won: ${scoreRound.computer} games.
-    You've tied: ${scoreRound.tie} games.`
+    messageArea.textContent = message;
+    setTimeout(displayMessage, 1000);
+    setTimeout(hideMessage, 4000);
+    
+    console.log(scoreRound);
+    return message;
+  };
+  
+function displayMessage() {
+  messageBox.setAttribute('style', 'visibility: visible');
+};
 
-    return roundResult;
+function hideMessage() {
+  messageBox.setAttribute('style', 'visibility: hidden');
+};
+
+function game() {
+ 
+  let i = 5;
+
+  while (i > 0) {
+    console.log(getScore());
+    i--;
   };
 
-  // let i = 5;
-
-  // while (i > 0) {
-  //   console.log(getScore());
-  //   i--;
-  // };
-
-  // if (scoreRound.player == scoreRound.computer) {
-  //   console.log(`Looks like it's a tie game. Play again?`);
-  // } else if (scoreRound.player > scoreRound.computer) {
-  //   console.log(`Congratulations! You won this match! Play again?`);
-  // } else {
-  //   console.log(`Sorry... The computer won this match. Better luck next time!`);
-  // }
+  if (scoreRound.player == scoreRound.computer) {
+    console.log(`Looks like it's a tie game. Play again?`);
+  } else if (scoreRound.player > scoreRound.computer) {
+    console.log(`Congratulations! You won this match! Play again?`);
+  } else {
+    console.log(`Sorry... The computer won this match. Better luck next time!`);
+  }
 };
 
